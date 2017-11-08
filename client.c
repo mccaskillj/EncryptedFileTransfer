8a9787d1f9661572a1b9cbd84791efdcec7af119
Zach Kipp <kipp.zach@gmail.com>
2017-11-07 19:48:10 -0700

    Parse command line arguments on client and server. Update Makefile and comments

--- client.c
+++ client.c
@@ -1,7 +1,56 @@
-#include <stdlib.h>
+/*
+ *  Group 3
+ *  Assignment #3 - Secure File Transfer
+ *  CMPT361 F17
+ *
+ *  Purpose: Client (txer) entry point.
+ */
 
-int main(int argc, char *argv[]) {
-  (void)argc;
-  (void)argv;
-  return 0;
+#include <getopt.h>
+#include <libgen.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "common.h"
+
+void usage(char *bin_path, int exit_status)
+{
+	char *bin = basename(bin_path);
+
+	fprintf(stderr,
+		"Usage: %s [-p port][-h]\n\n"
+		"Options:\n"
+		"-p Port to connect to server (default %s)\n"
+		"-h Help\n\n",
+		bin, DEFAULT_SERVER_PORT);
+	exit(exit_status);
+}
+
+int main(int argc, char *argv[])
+{
+	int opt = 0;
+	char *port = NULL;
+
+	while ((opt = getopt(argc, argv, "p:h")) != -1) {
+		switch (opt) {
+		case 'p':
+			port = strdup(optarg);
+			break;
+		case 'h':
+			usage(argv[0], EXIT_SUCCESS);
+		case ':':
+			usage(argv[0], EXIT_FAILURE);
+		case '?':
+			usage(argv[0], EXIT_FAILURE);
+		default:
+			usage(argv[0], EXIT_FAILURE);
+		}
+	}
+
+	if (NULL == port)
+		port = strdup(DEFAULT_SERVER_PORT);
+
+	free(port);
+	return EXIT_SUCCESS;
 }
